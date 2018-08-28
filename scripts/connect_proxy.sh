6baaffd80d44bbc2209a857e66850000673a0936
pl8787 <pl8787@gmail.com>
2018-08-28 16:42:58 +0800

    .

--- scripts/connect_proxy.sh
+++ scripts/connect_proxy.sh
@@ -4,7 +4,9 @@ NOT_CONNECT=$1
 if [ $NOT_CONNECT ]; then
     export http_proxy=""
     export https_proxy=""
+    echo "Disconnect Proxy."
 else
     export http_proxy=http://10.61.3.150:8088
     export https_proxy=http://10.61.3.150:8088
+    echo "Connect Proxy."
 fi
---
+++ scripts/test_tensorforce.sh
@@ -0,0 +1,13 @@
+set -e
+set -x
+P1="tensorforce::ppo"
+P2="test::agents.SimpleAgent"
+P3="test::agents.SimpleAgent"
+P4="test::agents.SimpleAgent" 
+pom_tf_battle \
+    --agents=$P1","$P2","$P3","$P4 \
+    --config=PommeFFACompetition-v0 #\
+    #--render \
+    #--render_mode rgb_array \
+    #--record_json_dir="../records/json/"
+
